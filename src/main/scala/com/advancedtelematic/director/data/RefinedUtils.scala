1028112d12a2dd3d5a32c02cd1bb5f30dec5a529
Simão Mata <simao@advancedtelematic.com>
2017-02-20 17:48:48 +0100

    review comments

--- src/main/scala/com/advancedtelematic/director/data/RefinedUtils.scala
+++ src/main/scala/com/advancedtelematic/director/data/RefinedUtils.scala
@@ -7,6 +7,7 @@ import org.genivi.sota.marshalling.RefinementError
 import scala.util.{Failure, Success, Try}
 
 object RefinedUtils {
+  // Like this, lets put it in libats
   implicit class RefineTry[T](value: T) {
     def refineTry[P](implicit ev: Validate[T, P]): Try[Refined[T, P]] = {
       refined.refineV(value) match {

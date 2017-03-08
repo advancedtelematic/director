1aaf8dda0dc8768eb478dbff92d49c9df12f64dd
Daniel Gustafsson <daniel@advancedtelematic.com>
2017-03-08 14:28:05 +0100

    make setTargets run in an transaction

---
+++ src/main/scala/com/advancedtelematic/director/db/SetTargets.scala
@@ -0,0 +1,26 @@
+package com.advancedtelematic.director.db
+
+import com.advancedtelematic.director.data.AdminRequest.SetTarget
+import com.advancedtelematic.director.data.DataType.{DeviceId, FileCacheRequest, Namespace, UpdateId}
+import com.advancedtelematic.director.data.FileCacheRequestStatus
+
+import scala.concurrent.{ExecutionContext, Future}
+import slick.driver.MySQLDriver.api._
+
+
+object SetTargets extends AdminRepositorySupport
+    with FileCacheRequestRepositorySupport {
+
+  def setTargets(namespace: Namespace, devTargets: Seq[(DeviceId, SetTarget)], updateId: Option[UpdateId] = None)
+                (implicit db: Database, ec: ExecutionContext): Future[Unit] = {
+    def devAction(device: DeviceId, targets: SetTarget): DBIO[Unit] =
+      adminRepository.updateTargetAction(namespace, device, updateId, targets.updates).flatMap { new_version =>
+        fileCacheRequestRepository.persistAction(FileCacheRequest(namespace, new_version, device,
+                                                                  FileCacheRequestStatus.PENDING))
+      }
+
+    db.run {
+      DBIO.sequence(devTargets.map((devAction _).tupled)).map(_ => ()).transactionally
+    }
+  }
+}

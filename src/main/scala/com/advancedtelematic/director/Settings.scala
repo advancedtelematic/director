e864d37c5a356bbe91f199d7e0e6e36439cf10a5
Simão Mata <simao.mata@here.com>
2020-06-08 15:59:00 +0200

    remove unused code
    Signed-off-by: Simão Mata <simao.mata@here.com>

--- src/main/scala/com/advancedtelematic/director/Settings.scala
+++ src/main/scala/com/advancedtelematic/director/Settings.scala
@@ -1,17 +1,16 @@
 package com.advancedtelematic.director
 
 import akka.event.Logging
+import akka.http.scaladsl.model.Uri
 import com.typesafe.config.ConfigFactory
 
 trait Settings {
-  import Util._
-
   private lazy val _config = ConfigFactory.load()
 
   val host = _config.getString("server.host")
   val port = _config.getInt("server.port")
 
-  val tufUri = mkUri(_config, "keyserver.uri")
+  val tufUri = Uri(_config.getString("keyserver.uri"))
 
   val requestLogLevel = Logging.levelFor(_config.getString("requestLogLevel")).getOrElse(Logging.DebugLevel)
 }

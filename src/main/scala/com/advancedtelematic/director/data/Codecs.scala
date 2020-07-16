4bf8c5eb5e744417dfb5d93c9acf49aa7356a10b
Jochen Schneider <j.schneider@here.com>
2020-07-16 13:26:30 +0200

    Forgot to rename the codec

--- src/main/scala/com/advancedtelematic/director/data/Codecs.scala
+++ src/main/scala/com/advancedtelematic/director/data/Codecs.scala
@@ -82,6 +82,6 @@ object Codecs {
 
   implicit val operationResultCodec: Codec[OperationResult] = deriveCodec
   implicit val ecuManifestCustomCodec: Codec[EcuManifestCustom] = deriveCodec
-  implicit val adminDeviceCodec: Codec[ClientDataType.Device] = deriveCodec
+  implicit val clientDeviceCodec: Codec[ClientDataType.Device] = deriveCodec
 }
 
